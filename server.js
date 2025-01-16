518bc7d4f1668249d745522a9dd4bc19919a58f2
maximus2497 <ayaansohailkhan786@gmail.com>
2025-01-16 22:42:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port $PORT`);
 });
 
 // Middleware
