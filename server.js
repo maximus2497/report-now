142c159929c12da8966975a3f3ec485d0ef44d19
maximus2497 <ayaansohailkhan786@gmail.com>
2025-01-16 22:47:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port $PORT`);
 });
 
